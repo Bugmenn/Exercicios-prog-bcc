b4c0ec2b4258e08763d4950b8ff15002469116f4
Bugmenn <125574692+Bugmenn@users.noreply.github.com>
2024-03-14 20:48:45 -0300

    Add files via upload

---
+++ Exercicios/src/aula_3_parte_2/Uni3Exe01.java
@@ -0,0 +1,21 @@
+package aula_3_parte_2;
+
+import java.util.Scanner;
+
+public class Uni3Exe01 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		
+		System.out.print("Escreva a altura:");
+		float altura = s.nextFloat();
+		
+		System.out.print("Escreva a largura:");
+		float largura = s.nextFloat();
+		
+		s.close();
+		
+		System.out.println("Área do terreno:"+largura*altura);
+	}
+
+}
