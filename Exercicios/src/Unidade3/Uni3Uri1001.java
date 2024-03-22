3cb491e07f46a7858cd34d844979620bc793a6b0
Bugmenn <125574692+Bugmenn@users.noreply.github.com>
2024-03-21 21:00:49 -0300

    Add files via upload

---
+++ Exercicios/src/Unidade3/Uni3Uri1001.java
@@ -0,0 +1,16 @@
+package Unidade3;
+
+import java.util.Scanner;
+
+public class Uni3Uri1001 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int a = s.nextInt();
+		int b = s.nextInt();
+		s.close();
+		int x = a+b;
+		System.out.println("X = "+x);
+	}
+
+}
